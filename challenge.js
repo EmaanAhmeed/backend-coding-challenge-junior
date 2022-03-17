3918643ea72d0ef8073cf94ec92c270b595d57cb
Eman Ahmed <emaan.ahmed1189@gmail.com>
2022-03-17 23:47:09 +0200

    solution

--- challenge.js
+++ challenge.js
@@ -8,4 +8,37 @@
  * @return {Array.<number>}
  */
 exports.getSalesByDay = function getSalesByDay(orders, startDate, endDate) {
+    let resArr=[];
+    let res = orders.reduce((result, item) => {
+        var date = new Date(item.date);
+        var noTime = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+        item.date = noTime;
+        if (result[noTime] == null) {
+            result[noTime] = []
+        }
+        result[noTime].push(item);
+       return result;
+    }, {});
+    let diff = endDate - startDate;
+    let numDays = Math.round(diff / (1000 * 60 * 60 * 24));
+    let day = new Date(startDate)
+    for (let i = 0; i < numDays; i++) {
+        let strDay=day.toString();
+        if(res[strDay]!= undefined){
+            let totalValues=0;
+            for(let j=0; j<res[strDay].length; j++){
+              totalValues+=res[strDay][j]['value'];
+            }
+            resArr.push(totalValues);
+        //   console.log(res[strDay]);  
+        }
+        else {
+resArr.push(0);
+        }
+        
+        day.setDate(day.getDate() + 1);
+    }
+    console.log(resArr);
+return resArr;
+
 }
